ad3559f1e83bfba91ce0bda75d37ab54de30ea16
mario <mario@DESKTOP-VIB8HDB>
2018-11-05 21:46:01 +0100

    tasca 1 acabada

--- public_html/code.js
+++ public_html/code.js
@@ -5,14 +5,58 @@
  */
 var mapa;
 var mapa_temp;
-var F1 = { x: 11, y: 15, sentit: 1 };
-var F2 = { x: 15, y: 16, sentit: 1 };
-var F3 = { x: 15, y: 14, sentit: 1 };
-var Player = { x: 9, y: 10, sentit: 1 };
+//0 up , 1 down, 2 right, 3 left
+var F1 = { x: 0, y: 0, sentit: 0 };
+var F2 = { x: 0, y: 0, sentit: 0 };
+var F3 = { x: 0, y: 0, sentit: 0 };
+var Player = { x: 0, y: 0, sentit: 0, gir: 0 };
 InicialitzarMapa();
-ColocarElements();
+InicialitzarJugadors();
+setInterval(Rellotge, 400);
+function Rellotge(){
+	MoureJugador();
+	ColocarElements();
+}
+function MoureJugador()
+{
+		if (Can(Player["x"], Player["y"], 	Player["gir"])) {
+			Player["sentit"]=Player["gir"];
+		}
+		if (Can(Player["x"], Player["y"], 	Player["sentit"])) 
+		{
+			switch (Player["sentit"]) {
+				case 0:
+				Player["x"]=Player["x"]-1;
+					break;
+				case 1:
+				Player["x"]=Player["x"]+1;
+					break;
+				case 2:
+				Player["y"]=Player["y"]+1;
+					break;
+				case 3:
+				Player["y"]=Player["y"]-1;
+					break;
+				default:
+					return false;
+					break;
+			}
+		}else
+		{
+			//Player["sentit"]=SentitAleatori();
+		}
+}
+function SentitAleatori(x,y,sentit)
+{
+	var sentits;
+	if(Can(x,y,0))
+	{
+		
+	}
+	return 1;
+}
 function InicialitzarMapa() {
-	var MapCode = "000000000000011111000000000000011111111111011111011111111110010101000011000100010100000010010101111111110101110111111110010100100000010101011100000010010100100011111111000111111110010111111110100001100000100000010101000010111000100111111110010101000010101111111100000010011101111110100100100111111110000101000010100100100100000100110111022011111111111111111101110001000010100200101010010001111111111110100200101010011111110001001010102220101010010001110111001010102220101011111101000100001010100000101000100100011100001010111111101011111110010111111110100000101010000010010100001011111111111110222010010100001010000100100010002010010111111111111111111111102010010100010101000000100000100010010100111111111111111111111110010100100000010101000000010010011111111111010101111111111110010010101001110101110010101010010010101001000100010010101010011111111111011111011111111110000000000000011111000000000000".split("");
+	var MapCode = "000000000000022222000000000000011111111111022222011111111110010101000001000200010100000010010101111111110201110111111110010100100000010101011100000010010100100011111111000111111110010111111110100101100000100000010101000010100100100111111110010101000010111111111100000010011101111110100100100111111110000101000010100100100100000100220111022011111111111111111102220001000010100000101010010002222211111110102220101010011222220001001010102220101010010002220111001010102220101011111102000100001010100000101000100100011100001010111111101011111110010111111110100000101010000010010100001011111111111110222010010100001010000100100010002010010111111111111111111111102010010100010101000000100000100010010100111111111111111111111110010100100000010101000000010010011111111111010201111111111110010010101001110201110010101010010010101001000200010010101010011111111111022222011111111110000000000000022222000000000000".split("");
 	mapa = new Array(30);
 	cont = 0;
 	contY = 0;
@@ -24,6 +68,36 @@ function InicialitzarMapa() {
 			cont++;
 		}
 	}
+}
+function Can(x, y, dir) {
+	switch (dir) {
+		case 0:
+			if (mapa[x - 1][y] != 2 && mapa[x - 1][y] != 0) {
+				return true;
+			} else return false;
+			break;
+		case 1:
+			if (mapa[x + 1][y] != 2 && mapa[x + 1][y] != 0) {
+				return true;
+			} else return false;
+			break;
+		case 2:
+			if (mapa[x][y + 1] != 2 && mapa[x][y + 1] != 0) {
+				return true;
+			} else return false;
+			break;
+		case 3:
+			if (mapa[x][y - 1] != 2 && mapa[x][y - 1] != 0) {
+				return true;
+			}
+			else return false;
+			break;
+		default:
+			return false;
+			break;
+	}
+}
+function InicialitzarJugadors() {
 	var x = 0;
 	var y = 0;
 	do {
@@ -57,8 +131,11 @@ function InicialitzarMapa() {
 	Player["x"] = x;
 	Player["y"] = y;
 
+
+
 }
 function MostrarMapa() {
+	console.clear();
 	var out = "";
 	for (var i = 0; i < 30; i++) {
 		for (var x = 0; x < 30; x++) {
@@ -69,17 +146,19 @@ function MostrarMapa() {
 			else if (mapa_temp[i][x] == 2)
 				out = out + "//";
 			else if (mapa_temp[i][x] == 3)
-				out = out + "F3";
+				out = out + "F1";
 			else if (mapa_temp[i][x] == 4)
-				out = out + "F4";
+				out = out + "F2";
 			else if (mapa_temp[i][x] == 5)
-				out = out + "F5";
+				out = out + "F3";
 			else if (mapa_temp[i][x] == 6)
 				out = out + "O:";
 		}
 		out = out + "\n";
 	}
 	console.log(out);
+
+
 }
 function NetejarMapa() {
 	mapa_temp = new Array(30);
@@ -90,6 +169,7 @@ function NetejarMapa() {
 			cont++;
 		}
 	}
+
 }
 function ColocarElements() {
 	NetejarMapa();
@@ -98,4 +178,25 @@ function ColocarElements() {
 	mapa_temp[F3["x"]][F3["y"]] = "5";
 	mapa_temp[Player["x"]][Player["y"]] = "6";
 	MostrarMapa();
+
+
+}
+
+function TeclaPitjada(e) {
+	if (e["code"] == "ArrowUp") {
+		Player["gir"]=0; 
+	
+	} else
+		if (e["code"] == "ArrowDown") {
+			Player["gir"]=1; 
+			
+		}
+	if (e["code"] == "ArrowRight") {
+		Player["gir"]=2; 
+		
+	}
+	if (e["code"] == "ArrowLeft") {
+		Player["gir"]=3; 
+	
+	}
 }
